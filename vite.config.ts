f1aaf2fb7943b33e88eb595345bbf61bec5bdc12
Chandra Raj Singh <chauhanvishal562@gmail.com>
2026-02-10 20:17:07 +0530

    settings updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: "/Neha/", // TODO: Replace <REPO_NAME> with your GitHub repository name
+    base: "/",
 });
